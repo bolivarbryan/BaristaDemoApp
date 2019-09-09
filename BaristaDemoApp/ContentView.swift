65847af6dfb25d70078f988019ba40ea7745d0ce
Bryan Bolivar <bryan.bolivar@ix.co>
2019-09-09 15:50:33 -0500

    Initial Commit

---
+++ BaristaDemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BaristaDemoApp
+//
+//  Created by Bryan Bolivar on 9/09/19.
+//  Copyright © 2019 Bryan Bolivar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
